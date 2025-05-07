0892fc02fe967fc8d382bd9dc189dc327b14ea76
Monir <mknishad1@ymail.com>
2025-05-07 15:34:54 +0600

    Configure dependency injection and create ProductApi in AppModule

---
+++ app/src/main/java/com/android/monir/ProductApp.kt
@@ -0,0 +1,7 @@
+package com.android.monir
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ProductApp: Application()
---
+++ app/src/main/java/com/android/monir/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.android.monir.di
+
+import com.android.monir.data.remote.ProductApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+  @Provides
+  @Singleton
+  fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+  }
+
+  @Provides
+  @Singleton
+  fun provideOkHttpClient(
+    authInterceptor: Interceptor, loggingInterceptor: HttpLoggingInterceptor
+  ): OkHttpClient =
+    OkHttpClient.Builder().addInterceptor(authInterceptor).addInterceptor(loggingInterceptor)
+      .build()
+
+  @Provides
+  @Singleton
+  fun provideProductApi(okHttpClient: OkHttpClient): ProductApi {
+    return Retrofit.Builder().baseUrl(ProductApi.BASE_URL)
+      .addConverterFactory(GsonConverterFactory.create()).client(okHttpClient).build()
+      .create(ProductApi::class.java)
+  }
+}
